dd48d240ede9c4035ae0db790724d32504ee6b59
iiamrosh <125737787+iiamrosh@users.noreply.github.com>
2026-08-03 23:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (37.87%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,26 +1,16 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-      int[] totals = addNums(accounts);
-        int max = 0;
-        for (int i = 0; i < totals.length; i++) {
-            if (totals[i]>max){
-                max = totals[i];
-            }
-        }
-        System.out.println(max);
-        return max;
-    }
-    
-    int[] addNums(int[][] accounts){
+      int ans = Integer.MIN_VALUE;
         int[] totalsArray = new int[accounts.length];
         for (int row = 0; row < accounts.length; row++) {
             int total=0;
             for (int col = 0; col < accounts[row].length; col++) {
                 total += accounts[row][col];
             }
-            totalsArray[row] = total ;
+            if (total>ans){
+                ans=total;
+            }
         }
-        System.out.println(Arrays.toString(totalsArray));
-        return totalsArray;
-    } 
+        return ans;
+    }
 }
